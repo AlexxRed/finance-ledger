1b0801f3162e559a56510a5d9ca7603fa201f6d5
Alexander Krasnobokyi <87702429+AlexxRed@users.noreply.github.com>
2023-01-25 10:37:38 +0200

    up

--- src/componets/App/App.jsx
+++ src/componets/App/App.jsx
@@ -1,6 +1,38 @@
 import './App.css';
 
 function App() {
+
+  const salaries =  {
+    Progger: { 
+    salary: 1000, 
+    tax: "15%" 
+    },
+    Tester: {
+    salary: 1000,
+    tax: "10%"
+    }
+    }
+
+    const team = [
+      {
+      name: "Masha",
+      specialization: "Progger" 
+      },
+      {
+      name: "Vasya",
+      specialization: "Tester"
+      },
+      {
+      name: "Taras",
+      specialization: "Tester"
+      },
+      ]
+
+  function calculateTeamFinanceReport(salaries, team){
+    console.log(salaries, team)
+  }
+  calculateTeamFinanceReport(salaries, team)
+
   return (
     <div className="App">
       <header className="App-header">
